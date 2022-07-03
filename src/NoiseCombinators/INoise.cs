379218975ee9702a565fef4959aeed65c88d8d11
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2022-07-03 16:51:12 +0200

    Adds interface methods for 3D noise

--- src/NoiseCombinators/INoise.cs
+++ src/NoiseCombinators/INoise.cs
@@ -154,4 +154,142 @@ public interface INoise
     /// <param name="stepsY">The number of steps to take in the y-axis.</param>
     /// <returns>The generated noise chunk.</returns>
     public Task<double[][]> GetChunk2DAsync(double x, double y, int stepsX, int stepsY);
+
+    /// <summary>
+    /// Gets a chunk of values at the given coordinates.
+    /// </summary>
+    /// <param name="seed">The seed to use for generating the noise.</param>
+    /// <param name="x">The starting x-coordinate.</param>
+    /// <param name="y">The starting y-coordinate.</param>
+    /// <param name="stepsX">The number of steps to take in the x-axis.</param>
+    /// <param name="stepsY">The number of steps to take in the y-axis.</param>
+    /// <param name="stepSizeX">The size of each step in the x-axis.</param>
+    /// <param name="stepSizeY">The size of each step in the y-axis.</param>
+    /// <returns>The generated noise chunk.</returns>
+    public double[][][] GetChunkWithSeed3D(int seed, double x, double y, int stepsX, int stepsY, double stepSizeX, double stepSizeY);
+
+    /// <summary>
+    /// Gets a chunk of values at the given coordinates.
+    /// </summary>
+    /// <param name="seed">The seed to use for generating the noise.</param>
+    /// <param name="x">The starting x-coordinate.</param>
+    /// <param name="y">The starting y-coordinate.</param>
+    /// <param name="stepsX">The number of steps to take in the x-axis.</param>
+    /// <param name="stepsY">The number of steps to take in the y-axis.</param>
+    /// <param name="stepSize">The size of each step in each axis.</param>
+    /// <returns>The generated noise chunk.</returns>
+    public double[][][] GetChunkWithSeed3D(int seed, double x, double y, int stepsX, int stepsY, double stepSize);
+
+    /// <summary>
+    /// Gets a chunk of values at the given coordinates.
+    /// </summary>
+    /// <param name="seed">The seed to use for generating the noise.</param>
+    /// <param name="x">The starting x-coordinate.</param>
+    /// <param name="y">The starting y-coordinate.</param>
+    /// <param name="stepsX">The number of steps to take in the x-axis.</param>
+    /// <param name="stepsY">The number of steps to take in the y-axis.</param>
+    /// <returns>The generated noise chunk.</returns>
+    public double[][][] GetChunkWithSeed3D(int seed, double x, double y, int stepsX, int stepsY);
+
+    /// <summary>
+    /// Gets a chunk of values at the given coordinates.
+    /// </summary>
+    /// <param name="x">The starting x-coordinate.</param>
+    /// <param name="y">The starting y-coordinate.</param>
+    /// <param name="stepsX">The number of steps to take in the x-axis.</param>
+    /// <param name="stepsY">The number of steps to take in the y-axis.</param>
+    /// <param name="stepSizeX">The size of each step in the x-axis.</param>
+    /// <param name="stepSizeY">The size of each step in the y-axis.</param>
+    /// <returns>The generated noise chunk.</returns>
+    public double[][][] GetChunk3D(double x, double y, int stepsX, int stepsY, double stepSizeX, double stepSizeY);
+
+    /// <summary>
+    /// Gets a chunk of values at the given coordinates.
+    /// </summary>
+    /// <param name="x">The starting x-coordinate.</param>
+    /// <param name="y">The starting y-coordinate.</param>
+    /// <param name="stepsX">The number of steps to take in the x-axis.</param>
+    /// <param name="stepsY">The number of steps to take in the y-axis.</param>
+    /// <param name="stepSize">The size of each step in each axis.</param>
+    /// <returns>The generated noise chunk.</returns>
+    public double[][][] GetChunk3D(double x, double y, int stepsX, int stepsY, double stepSize);
+
+    /// <summary>
+    /// Gets a chunk of values at the given coordinates.
+    /// </summary>
+    /// <param name="x">The starting x-coordinate.</param>
+    /// <param name="y">The starting y-coordinate.</param>
+    /// <param name="stepsX">The number of steps to take in the x-axis.</param>
+    /// <param name="stepsY">The number of steps to take in the y-axis.</param>
+    /// <returns>The generated noise chunk.</returns>
+    public double[][][] GetChunk3D(double x, double y, int stepsX, int stepsY);
+
+    /// <summary>
+    /// Gets a chunk of values at the given coordinates.
+    /// </summary>
+    /// <param name="seed">The seed to use for generating the noise.</param>
+    /// <param name="x">The starting x-coordinate.</param>
+    /// <param name="y">The starting y-coordinate.</param>
+    /// <param name="stepsX">The number of steps to take in the x-axis.</param>
+    /// <param name="stepsY">The number of steps to take in the y-axis.</param>
+    /// <param name="stepSizeX">The size of each step in the x-axis.</param>
+    /// <param name="stepSizeY">The size of each step in the y-axis.</param>
+    /// <returns>The generated noise chunk.</returns>
+    public Task<double[][][]> GetChunkWithSeed3DAsync(int seed, double x, double y, int stepsX, int stepsY, double stepSizeX, double stepSizeY);
+
+    /// <summary>
+    /// Gets a chunk of values at the given coordinates.
+    /// </summary>
+    /// <param name="seed">The seed to use for generating the noise.</param>
+    /// <param name="x">The starting x-coordinate.</param>
+    /// <param name="y">The starting y-coordinate.</param>
+    /// <param name="stepsX">The number of steps to take in the x-axis.</param>
+    /// <param name="stepsY">The number of steps to take in the y-axis.</param>
+    /// <param name="stepSize">The size of each step in each axis.</param>
+    /// <returns>The generated noise chunk.</returns>
+    public Task<double[][][]> GetChunkWithSeed3DAsync(int seed, double x, double y, int stepsX, int stepsY, double stepSize);
+
+    /// <summary>
+    /// Gets a chunk of values at the given coordinates.
+    /// </summary>
+    /// <param name="seed">The seed to use for generating the noise.</param>
+    /// <param name="x">The starting x-coordinate.</param>
+    /// <param name="y">The starting y-coordinate.</param>
+    /// <param name="stepsX">The number of steps to take in the x-axis.</param>
+    /// <param name="stepsY">The number of steps to take in the y-axis.</param>
+    /// <returns>The generated noise chunk.</returns>
+    public Task<double[][][]> GetChunkWithSeed3DAsync(int seed, double x, double y, int stepsX, int stepsY);
+
+    /// <summary>
+    /// Gets a chunk of values at the given coordinates.
+    /// </summary>
+    /// <param name="x">The starting x-coordinate.</param>
+    /// <param name="y">The starting y-coordinate.</param>
+    /// <param name="stepsX">The number of steps to take in the x-axis.</param>
+    /// <param name="stepsY">The number of steps to take in the y-axis.</param>
+    /// <param name="stepSizeX">The size of each step in the x-axis.</param>
+    /// <param name="stepSizeY">The size of each step in the y-axis.</param>
+    /// <returns>The generated noise chunk.</returns>
+    public Task<double[][][]> GetChunk3DAsync(double x, double y, int stepsX, int stepsY, double stepSizeX, double stepSizeY);
+
+    /// <summary>
+    /// Gets a chunk of values at the given coordinates.
+    /// </summary>
+    /// <param name="x">The starting x-coordinate.</param>
+    /// <param name="y">The starting y-coordinate.</param>
+    /// <param name="stepsX">The number of steps to take in the x-axis.</param>
+    /// <param name="stepsY">The number of steps to take in the y-axis.</param>
+    /// <param name="stepSize">The size of each step in each axis.</param>
+    /// <returns>The generated noise chunk.</returns>
+    public Task<double[][][]> GetChunk3DAsync(double x, double y, int stepsX, int stepsY, double stepSize);
+
+    /// <summary>
+    /// Gets a chunk of values at the given coordinates.
+    /// </summary>
+    /// <param name="x">The starting x-coordinate.</param>
+    /// <param name="y">The starting y-coordinate.</param>
+    /// <param name="stepsX">The number of steps to take in the x-axis.</param>
+    /// <param name="stepsY">The number of steps to take in the y-axis.</param>
+    /// <returns>The generated noise chunk.</returns>
+    public Task<double[][][]> GetChunk3DAsync(double x, double y, int stepsX, int stepsY);
 }
